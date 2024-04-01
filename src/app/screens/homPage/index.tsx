e8ab07b6bd58186cb77124177a5a2f55d06a559c
metablog <khu10735@naver.com>
2024-04-02 00:02:03 +0900

    fix: organize hompage related sectional components

--- src/app/screens/homPage/index.tsx
+++ src/app/screens/homPage/index.tsx
@@ -1,5 +1,19 @@
-import { Container } from "@mui/material";
+import Statistics from "../helpPage/Statistics";
+import PopularDishes from "../helpPage/PopularDishes";
+import NewDishes from "../helpPage/NewDishes";
+import Advertisement from "../helpPage/Advertisement";
+import ActiveUsers from "../helpPage/ActiveUsers";
+import Events from "../helpPage/Events";
 
 export default function HomePage() {
-  return <Container>HomePage</Container>;
+  return (
+    <div className="homepage">
+      <Statistics />
+      <PopularDishes />
+      <NewDishes />
+      <Advertisement />
+      <ActiveUsers />
+      <Events />
+    </div>
+  );
 }
