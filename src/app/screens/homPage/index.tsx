9c7d77b41f0070f5cb7754f24ae7284014af9e1a
metablog <khu10735@naver.com>
2024-04-22 01:17:35 +0900

    feat: develop slice and selector usege

--- src/app/screens/homPage/index.tsx
+++ src/app/screens/homPage/index.tsx
@@ -1,13 +1,30 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import Statistics from "./Statistics";
 import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setPopularDishes } from "./slice";
+import { retrievePopularDishes } from "./selector";
 import "../../css/home.css";
+import { Product } from "../../../lib/types/product";
+
+const actionDispatch = (dispatch: Dispatch) => ({
+  setPopularDishes: (data: Product) => dispatch(setPopularDishes(data)),
+});
+
+const popularDishesRetriever = createSelector(
+  retrievePopularDishes,
+  (popularDishes) => ({ popularDishes })
+);
 
 export default function HomePage() {
+  const { setPopularDishes } = actionDispatch(useDispatch());
+  const { popularDishes } = useSelector(popularDishesRetriever);
   useEffect(() => {}, []);
 
   return (
