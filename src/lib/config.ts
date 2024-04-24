3c67cc398d6b8aa078304eafc4850e17eb758971
metablog <khu10735@naver.com>
2024-04-24 13:27:46 +0900

    feat: create config & env variables

---
+++ src/lib/config.ts
@@ -0,0 +1,9 @@
+export const serverApi: string = `${process.env.REACT_APP_URL}`;
+
+export const Message = {
+  erro1: "Something went wrong!",
+  erro2: "Please login first!",
+  erro3: "Please fulfill all inputs!",
+  erro4: "Message is empty!",
+  erro5: "Only images with jpeg, jpg, png format allowed!",
+};
