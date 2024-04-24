5a580f2db0f45e4b9e034daa8068175fef034ebe
metablog <khu10735@naver.com>
2024-04-24 14:17:21 +0900

    feat: develop porudctService business logic to retreive from backend

--- src/app/screens/homPage/index.tsx
+++ src/app/screens/homPage/index.tsx
@@ -5,27 +5,36 @@ import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
-import { useDispatch, useSelector } from "react-redux";
+
+import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { createSelector } from "reselect";
 import { setPopularDishes } from "./slice";
-import { retrievePopularDishes } from "./selector";
 import "../../css/home.css";
 import { Product } from "../../../lib/types/product";
+import ProductService from "../../services/ProductService";
+import { ProductCollection } from "../../../lib/enums/product.enum";
 
 const actionDispatch = (dispatch: Dispatch) => ({
-  setPopularDishes: (data: Product) => dispatch(setPopularDishes(data)),
+  setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
 });
-
-const popularDishesRetriever = createSelector(
-  retrievePopularDishes,
-  (popularDishes) => ({ popularDishes })
-);
 
 export default function HomePage() {
   const { setPopularDishes } = actionDispatch(useDispatch());
-  const { popularDishes } = useSelector(popularDishesRetriever);
-  useEffect(() => {}, []);
+
+  useEffect(() => {
+    const product = new ProductService();
+    product
+      .getProduct({
+        page: 1,
+        limit: 4,
+        order: "productViews",
+        productCollection: ProductCollection.DISH,
+      })
+      .then((data) => {
+        setPopularDishes(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className="homepage">
